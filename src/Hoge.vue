20a7111c3f66092c0a815458a034669fb5e398ff
zaru@sakuraba <zarutofu@gmail.com>
2017-04-20 10:47:30 +0900

    Added hoge.vue files

---
+++ src/Hoge.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+div#hoge
+  p hoge text
+  p {{ message }}
+  | plain text
+  ul(v-for="item in items")
+    li {{ item }}
+</template>
+
+<script>
+export default {
+  name: 'hoge',
+  data () {
+    return {
+      message: 'hoge piyo',
+      items: []
+    }
+  },
+  created () {
+    this.fetch_json("./hoge.json");
+  },
+  methods: {
+    fetch_json(url) {
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", url);
+      xhr.responseType = "json";
+      xhr.addEventListener("loadend", this.set_data);
+      xhr.send();
+    },
+    set_data(e) {
+      this.items = e.target.response;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/hoge.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Hoge from './Hoge.vue'
+
+new Vue({
+  el: '#hoge',
+  render: h => h(Hoge)
+})
