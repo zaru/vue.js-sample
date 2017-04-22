4b86a7a49e49ec02d69161253a06194df5b30359
zaru@sakuraba <zarutofu@gmail.com>
2017-04-22 23:46:51 +0900

    Added delete button

--- src/Todo.vue
+++ src/Todo.vue
@@ -3,7 +3,9 @@ div#todo
   p
     input(type="text" v-model="task" v-on:keydown.enter="submit" autofocus)
   ul
-    li(v-for="item in items") {{ item }}
+    li(v-for="item in items")
+      |{{ item.title }}
+      button(v-on:click="task_delete(item)") [x]
 </template>
 
 <script>
@@ -12,15 +14,33 @@ export default {
   data () {
     return {
       task: "",
-      items: []
+      items: {}
     }
   },
   created () {
   },
   methods: {
     submit (e) {
-      this.items.push(this.task);
+      let uuid = this.uuid();
+      this.items[uuid] = {
+        id: uuid,
+        title: this.task
+      };
       this.task = "";
+    },
+    task_delete (todo) {
+      this.$delete(this.items, todo.id);
+    },
+    uuid () {
+      var uuid = "", i, random;
+      for (i = 0; i < 32; i++) {
+        random = Math.random() * 16 | 0;
+        if (i == 8 || i == 12 || i == 16 || i == 20) {
+          uuid += "-"
+        }
+        uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
+      }
+      return uuid;
     }
   }
 }
